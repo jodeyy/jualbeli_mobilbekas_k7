39cb79ace3b97849ba93415238dd0a92ddfc69de
IlhamSwd <160827594+IlhamSwd@users.noreply.github.com>
2024-12-15 15:06:20 +0700

    membuat router di MainScreen
    Berisi Home Screen, searchScreen,FavoriteScreen, ProfilScree, sign in/up

--- lib/main.dart
+++ lib/main.dart
@@ -33,13 +33,81 @@ class MyApp extends StatelessWidget {
 
       // home: DetailScreen(),
       // home: ProfileScreen(),
-      home: HomeScreen(),
+      // home: HomeScreen(),
       //home: ChatScreen(),
       //home: SignInScreen(),
       //home: SignUpScreen(),
        //home: SearchScreen(),
       //home: FavoriteScreen(),
+      home: MainScreen(),
+      initialRoute: '/',
+      routes: {
+        '/homescreen' : (context) => const HomeScreen(),
+        '/signin' : (context) =>  SignInScreen(),
+        '/signup' : (context) =>  SignUpScreen(),
+      },
     );
   }
 }
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  //deklarasi Variabel
+  int _currentIndex = 0;
+  
+  final List<Widget> _children = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const FavoriteScreen(),
+    const ProfileScreen(),
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  _children[_currentIndex],
+      bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: Colors.blue[50],
+          ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index){
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.blue,),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search, color: Colors.blue,),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite, color: Colors.blue,),
+              label: 'Favorite',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person, color: Colors.blue,),
+              label: 'Profile',
+            ),
+          ],
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.blue[100],
+          showUnselectedLabels: true,
+        ),
+      ),
+    );
+  }
+}
+
 
--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:jualbelimobil/data/mobil_data.dart';
 
-class FavoriteScreen extends StatelessWidget {
-  final List<String> favoriteItems;
-
-  FavoriteScreen({required this.favoriteItems});
+class FavoriteScreen extends StatefulWidget {
+  const FavoriteScreen({super.key});
 
   @override
+  State<FavoriteScreen> createState() => _FavoriteScreenState();
+}
+
+class _FavoriteScreenState extends State<FavoriteScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Favorite Cars'),
-      ),
-      body: favoriteItems.isEmpty
-          ? Center(child: Text('Belum ada mobil favorit.'))
-          : ListView.builder(
-        itemCount: favoriteItems.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: Icon(Icons.car_rental, color: Colors.blue),
-            title: Text(favoriteItems[index]),
-          );
-        },
-      ),
-    );
+    return const Placeholder();
   }
 }
+
+
+// class FavoriteScreen extends StatelessWidget {
+//   final List<String> favoriteItems;
+//
+//   FavoriteScreen({required this.favoriteItems});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Favorite Cars'),
+//       ),
+//       body: favoriteItems.isEmpty
+//           ? Center(child: Text('Belum ada mobil favorit.'))
+//           : ListView.builder(
+//         itemCount: favoriteItems.length,
+//         itemBuilder: (context,d index) {
+//           return ListTile(
+//             leading: Icon(Icons.car_rental, color: Colors.blue),
+//             title: Text(favoriteItems[index]),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
