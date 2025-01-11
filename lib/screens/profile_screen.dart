8d53f1072de1ef11e479454e023509449d25888c
Ilham Swd <160827594+IlhamSwd@users.noreply.github.com>
2025-01-11 21:58:18 +0700

    perbaikan edit profil sceeen dan integrasi kamera

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -13,9 +13,14 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  // Variabel profil
   String _imageFile = '';
   final picker = ImagePicker();
+
+  String fullName = '';
+  String userName = '';
+  String email = '';
+  String phoneNumber = '';
+  bool isSignedIn = false;
 
   Future<void> _saveImage() async {
     final prefs = await SharedPreferences.getInstance();
@@ -67,7 +72,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 leading: const Icon(Icons.camera_enhance, color: Colors.blue),
                 title: const Text('Camera'),
                 onTap: () {
-                  debugPrint('Kamera dipanggil');
                   Navigator.of(context).pop();
                   _getImage(ImageSource.camera);
                 },
@@ -87,43 +91,76 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
-  bool isSignedIn = false;
-  String fullName = '';
-  String userName = '';
-  String email = '';
-  String phoneNumber = '';
-
-  // Fungsi untuk mendekripsi data dari SharedPreferences
   Future<void> _loadProfileData() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String? keyString = prefs.getString('key'); // Ambil kunci enkripsi
-    final String? ivString = prefs.getString('iv'); // Ambil IV enkripsi
+    final String? keyString = prefs.getString('key');
+    final String? ivString = prefs.getString('iv');
 
     if (keyString != null && ivString != null) {
       final key = encrypt.Key.fromBase64(keyString);
       final iv = encrypt.IV.fromBase64(ivString);
       final encrypter = encrypt.Encrypter(encrypt.AES(key));
 
-      // Dekripsi setiap data yang terenkripsi
       setState(() {
         fullName = encrypter.decrypt64(prefs.getString('name') ?? '', iv: iv);
         userName = encrypter.decrypt64(prefs.getString('username') ?? '', iv: iv);
         email = encrypter.decrypt64(prefs.getString('email') ?? '', iv: iv);
         phoneNumber = encrypter.decrypt64(prefs.getString('notelpon') ?? '', iv: iv);
-        isSignedIn = fullName.isNotEmpty && userName.isNotEmpty;
-      });
-    } else {
-      // Jika tidak ada data terenkripsi, set profil kosong
-      setState(() {
-        isSignedIn = false;
+        isSignedIn = true;
       });
     }
   }
 
-  // Fungsi Sign Out
-  Future<void> signOut() async {
+  Future<void> _saveProfileData() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.clear(); // Menghapus semua data saat sign out
+    final String? keyString = prefs.getString('key');
+    final String? ivString = prefs.getString('iv');
+
+    if (keyString != null && ivString != null) {
+      final key = encrypt.Key.fromBase64(keyString);
+      final iv = encrypt.IV.fromBase64(ivString);
+      final encrypter = encrypt.Encrypter(encrypt.AES(key));
+
+      prefs.setString('name', encrypter.encrypt(fullName, iv: iv).base64);
+      prefs.setString('username', encrypter.encrypt(userName, iv: iv).base64);
+      prefs.setString('email', encrypter.encrypt(email, iv: iv).base64);
+      prefs.setString('notelpon', encrypter.encrypt(phoneNumber, iv: iv).base64);
+    }
+  }
+
+  void _editField(String label, String initialValue, Function(String) onSave) {
+    TextEditingController controller = TextEditingController(text: initialValue);
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Edit $label'),
+        content: TextField(
+          controller: controller,
+          decoration: InputDecoration(hintText: 'Enter new $label'),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              onSave(controller.text);
+              _saveProfileData();
+              Navigator.of(context).pop();
+            },
+            child: const Text('Save'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _signOut() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
     setState(() {
       isSignedIn = false;
       fullName = '';
@@ -132,19 +169,13 @@ class _ProfileScreenState extends State<ProfileScreen> {
       phoneNumber = '';
       _imageFile = '';
     });
-    Navigator.pushReplacementNamed(context, '/signin');
-  }
-
-  // Navigasi ke halaman Sign In
-  void signIn() {
-    Navigator.pushNamed(context, '/signin');
   }
 
   @override
   void initState() {
     super.initState();
-    _loadProfileData(); // Muat data saat layar dibuka
-    _loadImage(); // Muat gambar profil saat layar dibuka
+    _loadProfileData();
+    _loadImage();
   }
 
   @override
@@ -161,7 +192,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               children: [
-                // Gambar Profil
                 Align(
                   alignment: Alignment.topCenter,
                   child: Padding(
@@ -192,25 +222,19 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   ),
                 ),
                 const SizedBox(height: 20),
-                // Informasi Profil
                 Divider(color: Colors.blue[100]),
-                _buildProfileRow('Nama', fullName, Icons.person, isSignedIn),
+                _buildEditableRow('Nama', fullName, Icons.person, (value) => setState(() => fullName = value)),
                 Divider(color: Colors.blue[100]),
-                _buildProfileRow('Username', userName, Icons.account_box, isSignedIn),
+                _buildEditableRow('Username', userName, Icons.account_box, (value) => setState(() => userName = value)),
                 Divider(color: Colors.blue[100]),
-                _buildProfileRow('Email', email, Icons.email, isSignedIn),
+                _buildEditableRow('Email', email, Icons.email, (value) => setState(() => email = value)),
                 Divider(color: Colors.blue[100]),
-                _buildProfileRow('Telepon', phoneNumber, Icons.phone, isSignedIn),
+                _buildEditableRow('Telepon', phoneNumber, Icons.phone, (value) => setState(() => phoneNumber = value)),
                 const SizedBox(height: 20),
-                // Tombol Sign In/Out
-                isSignedIn
-                    ? TextButton(
-                      onPressed: signOut,
-                      child: const Text('Sign Out'),
-                )
-                    : TextButton(
-                      onPressed: signIn,
-                      child: const Text('Sign In'),
+                Divider(color: Colors.blue[100]),
+                TextButton(
+                  onPressed: isSignedIn ? _signOut : () => Navigator.pushNamed(context, '/signin'),
+                  child: Text(isSignedIn ? 'Logout' : 'Login'),
                 ),
               ],
             ),
@@ -220,23 +244,20 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
-  // Widget untuk membuat baris profil
-  Widget _buildProfileRow(String label, String value, IconData icon, bool isEditable) {
+  Widget _buildEditableRow(String label, String value, IconData icon, Function(String) onSave) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        SizedBox(
-          width: MediaQuery.of(context).size.width / 3,
-          child: Row(
-            children: [
-              Icon(icon, color: Colors.blue),
-              const SizedBox(width: 8),
-              Text(label,
-                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            ],
-          ),
+        Row(
+          children: [
+            Icon(icon, color: Colors.blue),
+            const SizedBox(width: 8),
+            Text('$label: $value', style: const TextStyle(fontSize: 18)),
+          ],
         ),
-        Expanded(
-          child: Text(': $value', style: const TextStyle(fontSize: 18)),
+        IconButton(
+          icon: const Icon(Icons.edit, color: Colors.blue),
+          onPressed: () => _editField(label, value, onSave),
         ),
       ],
     );
